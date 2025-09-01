c30584794997c79ba2c55bb6481729d1ebeb7556
Yurii <yurij.kenu@gmail.com>
2025-09-01 11:14:08 +0200

    create-person-class: added new Person class and create_person_list

--- app/main.py
+++ app/main.py
@@ -1,8 +1,32 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    results = []
+    for person in people:
+        new_person = Person(person["name"], person["age"])
+        results.append(new_person)
+
+    for person in people:
+        if "wife" in person and person["wife"] is not None:
+            new_person = next(
+                item for item in results if item.name == person["name"]
+            )
+            new_person.wife = next(
+                item for item in results if item.name == person["wife"]
+            )
+
+        if "husband" in person and person["husband"] is not None:
+            new_person = next(
+                item for item in results if item.name == person["name"]
+            )
+            new_person.husband = next(
+                item for item in results if item.name == person["husband"]
+            )
+    return results
